c6c4845083d203cdb3eaf45c723d9e34fe1bbace
Laurence Liang <laurence.w.liang@gmail.com>
2024-06-26 20:47:31 -0400

    Workable code

--- client.py
+++ client.py
@@ -2,10 +2,11 @@
 Client program
 """
 
-import requests
-import interface as itfc
+from serveapi import interface as itfc
 
 if __name__ == '__main__':
+    print('client')
     np = itfc.ServeApi2('np')
+    print('client')
     resp = np.linalg.norm([100,200,300])
     print(resp)
