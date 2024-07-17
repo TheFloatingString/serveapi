8af5f5a1dc76f961fd602d35c0593c667622c431
Laurence Liang <laurence.w.liang@gmail.com>
2024-07-17 19:42:05 -0400

    Merge classes together

--- client.py
+++ client.py
@@ -4,9 +4,15 @@ Client program
 
 from serveapi import interface as itfc
 
-if __name__ == '__main__':
-    print('client')
-    np = itfc.ServeApi2('np')
-    print('client')
-    resp = np.linalg.norm([100,200,300])
+
+if __name__ == "__main__":
+    transformers = itfc.DynamicModule("transformers")
+    cap = transformers.pipeline(model="ydshieh/vit-gpt2-coco-en")
+    print(cap)
+    x = "https://images.unsplash.com/photo-1582979512210-99b6a53386f9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    tmp = cap(x)
+    print(tmp)
+
+    np = itfc.DynamicModule("np")
+    resp = np.linalg.norm([100, 200, 300])
     print(resp)
--- serveapi/interface.py
+++ serveapi/interface.py
@@ -1,33 +1,73 @@
 import requests
 
-class Var2():
-    def __init__(self):
-        self.server_varname = None
-        self.primitive_value = None
+
+class Var:
+    def __init__(self, value, varname=None):
+        self.value = value
+        self.varname = varname
 
     def __repr__(self):
-        return self.primitive_value
-
-class ServeApi2():
-    def __init__(self, attr_name: str, depth_zero:bool=True):
-        self.attr_name = attr_name
-        self.str_literal = ''
+        return f"{self.varname} = {self.value}"
 
     def __call__(self, *args, **kwargs):
-        resp = requests.post('http://127.0.0.1:8000/api/eval', json={'data':self.attr_name+str(args)})
+        command = f"vars_dict['{self.varname}']" + str(args)
+        resp = requests.post("http://127.0.0.1:8000/api/eval", json={"data": command})
         eval_resp = eval(resp.json()["data"])
-        return eval_resp
- 
-    def __getattr__(self, name: str):
-        self.str_literal += name
-        return ServeApi2(self.attr_name + '.' + name, depth_zero=False)
+        return_var = Var(value=eval_resp, varname=resp.json()["varname"])
+        return return_var
 
-class CreateVar():
+
+class DynamicModule:
     def __init__(self, attr_name: str):
         self.attr_name = attr_name
+        self.str_literal = ""
 
     def __call__(self, *args, **kwargs):
-        exec_str = self.attr_name+str(args)
-        print(exec_str)
-        resp = requests.post('http://127.0.0.1:8000/api/exec', json={'data':exec_str})
-        return True
+        if len(args) > 0 and type(args[0]) == Var:
+            command = self.attr_name + f"({args[0].varname})"
+            resp = requests.post(
+                "http://127.0.0.1:8000/api/eval", json={"data": command}
+            )
+        if len(kwargs) > 0 and len(args) == 0:
+            modified_kwargs_literal = "("
+            for key_ in kwargs.keys():
+                modified_kwargs_literal += f"{key_}='{kwargs[key_]}',"
+            modified_kwargs_literal = modified_kwargs_literal[:-1] + ")"
+            full_eval_literal = self.attr_name + modified_kwargs_literal
+            print(full_eval_literal)
+            resp = requests.post(
+                "http://127.0.0.1:8000/api/eval",
+                json={"data": full_eval_literal},
+            )
+        else:
+            resp = requests.post(
+                "http://127.0.0.1:8000/api/eval",
+                json={"data": self.attr_name + str(args)},
+            )
+        try:
+            eval_resp = eval(resp.json()["data"])
+        except:
+            eval_resp = str(resp.json()["data"])
+        return_var = Var(value=eval_resp, varname=resp.json()["varname"])
+        return return_var
+
+    def __getattr__(self, name: str):
+        self.str_literal += name
+        return DynamicModule(self.attr_name + "." + name)
+
+
+# class StaticModule:
+#     def __init__(self, module_name) -> DynamicModule:
+#         self.module_name = module_name
+#         return
+
+
+# class CreateVar():
+#     def __init__(self, attr_name: str):
+#         self.attr_name = attr_name
+
+#     def __call__(self, *args, **kwargs):
+#         exec_str = self.attr_name+str(args)
+#         print(exec_str)
+#         resp = requests.post('http://127.0.0.1:8000/api/exec', json={'data':exec_str})
+#         return True
